e51fcdaf45e6487536f5b7c4440c8db37cd9ceee
Jin An <jin.an@dtcc.edu>
2022-01-25 15:55:46 -0500

    tue0125

---
+++ Week1-Pokedex/TueJan25/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Main {
+    /*
+    Import/read through pokemon.csv
+    As we look through that file, create a bunch of Pokemon objects
+    Put each of those pokemon objects inside an ArrayList
+     */
+    public static void main(String[] args) {
+        ArrayList<Pokemon> pokemonList = readCSV("pokemon.csv");
+    }
+
+    private static ArrayList<Pokemon> readCSV(String filename) {
+
+
+        return null;
+    }
+
+    // readCSV method
+    // takes a String as a parameter
+    // return type is an ArrayList<Pokemon>
+}
---
+++ Week1-Pokedex/TueJan25/Pokemon.java
@@ -0,0 +1,9 @@
+public class Pokemon {
+    // define what a pokemon is
+    // data fields
+
+    // and what a pokemon does
+    // methods
+
+    // possibly some constructors
+}
