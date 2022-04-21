5071833a7c09a8fa2aceadf5aa9f05c2dc778b3b
Jin An <jin.an@dtcc.edu>
2022-04-21 14:36:06 -0400

    week12thu

---
+++ Week12-GraphAdjList/Main.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        Graph<Integer> graph = new Graph<>(7);
+
+        for (int i = 0; i < 7; i++) {
+            graph.addVertex(i);
+        }
+
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(2, 1);
+        graph.addEdge(3, 1);
+        graph.addEdge(2, 4);
+        graph.addEdge(3, 4);
+        graph.addEdge(4, 5);
+        graph.addEdge(5, 6);
+
+
+        System.out.println(graph.getNeighbors(0));
+        // 1 and 2
+
+        graph.depthFirstTraversal(0);
+        // 6 5 4 3 2 1 0
+        // we can take advantage of stack
+
+        // graph.breadthFirstTraversal(0);
+        // 0 1 2 3 4 5 6
+        // we can take advantage of queue
+
+    }
+}
