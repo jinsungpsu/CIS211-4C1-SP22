b384967e00165c5c43f9a12d1590b760669fbdc8
Jin An <jin.an@dtcc.edu>
2022-04-26 15:07:14 -0400

    dfs

---
+++ Week13-GraphTraversal/Graph.java
@@ -0,0 +1,115 @@
+import java.util.*;
+
+public class Graph<T> {
+    private LinkedList[] adjList;
+    private HashMap<T, Integer> vertices = new HashMap<>();
+    private HashMap<Integer, T> revVerticesMap = new HashMap<>();
+    private int count = 0;
+
+    public Graph(int numVertices) {
+        adjList = new LinkedList[numVertices];
+    }
+
+    public void addVertex(T vertex) {
+        vertices.put(vertex, count);
+        revVerticesMap.put(count, vertex);
+        count++;
+    }
+
+    public void addEdge(T src, T dest) {
+        if (adjList[vertices.get(src)] == null) {
+            adjList[vertices.get(src)] = new LinkedList();
+        }
+
+        boolean destExists = false;
+        for (int i = 0; i < adjList[vertices.get(src)].size(); i++) {
+            if (adjList[vertices.get(src)].get(i).equals(dest)) {
+                // edge already exists
+                destExists = true;
+            }
+        }
+        if (!destExists)
+            adjList[vertices.get(src)].add(dest);
+
+
+        // in a directed graph, we can take this whole second section out
+        if (adjList[vertices.get(dest)] == null) {
+            adjList[vertices.get(dest)] = new LinkedList();
+        }
+
+        for (int i = 0; i < adjList[vertices.get(dest)].size(); i++) {
+            if (adjList[vertices.get(dest)].get(i).equals(vertices.get(src))) {
+                // edge already exists
+                return;
+            }
+        }
+        adjList[vertices.get(dest)].add(vertices.get(src));
+
+    }
+
+    public List<T> getNeighbors(T vertex) {
+        int index = vertices.get(vertex);
+        LinkedList neighbors = adjList[index];
+
+//        T[] neighborsArr = (T[])new Object[neighbors.size()] ;
+//
+//        for (int i = 0; i < neighbors.size(); i++) {
+//            neighborsArr[i] = revVerticesMap.get(neighbors.get(i));
+//        }
+//
+//        return neighborsArr;
+        ArrayList<T> neighborsArr = new ArrayList<>();
+        for (int i = 0; i < neighbors.size(); i++) {
+            neighborsArr.add(revVerticesMap.get(neighbors.get(i)));
+        }
+
+        return neighborsArr;
+    }
+
+    public void depthFirstTraversal(T src) {
+
+        // https://favtutor.com/blogs/depth-first-search-java
+        // this implementation used integers
+        // had to make changes to make it generic
+        // by using our hash maps
+
+        // ArrayList<T> visited = new ArrayList<>();
+        // this would have been very inefficient
+        // because we would have had to do a linear search
+        // every time we wanted to check if a vertex was visited
+
+        boolean[] visited = new boolean[adjList.length];
+        // an array of booleans is much more efficient!
+
+        Stack<T> verticesStack = new Stack<>();
+
+        verticesStack.push(src);
+        visited[vertices.get(src)] = true;;
+
+        // loop, is my stack empty?
+        while(!verticesStack.isEmpty()) {
+
+            T whereAmINow = verticesStack.pop();
+
+            if (visited[vertices.get(whereAmINow)] == false) {
+                System.out.println("I am at: " + whereAmINow);
+                visited[vertices.get(whereAmINow)] = true;
+            }
+
+            int index = vertices.get(whereAmINow);
+            LinkedList<T> neighbors = adjList[index];
+
+            for (int i = 0; i < neighbors.size(); i++) {
+                T neighbor = neighbors.get(i);
+                int indexOfNeighbor = vertices.get(neighbor);
+                if (visited[indexOfNeighbor] == false) {
+                    verticesStack.push(neighbor);
+                }
+            }
+            // if all neighbors are visited, we pop from the stack
+            // take next neighbor and push it on to the stack
+            // update the visited list
+            // "process"
+        }
+    }
+}
